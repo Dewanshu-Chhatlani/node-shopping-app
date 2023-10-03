3a42415d97104c2c4268ebf8c2e92587ddac4503
Dewanshu Chhatlani <devanshu.chh@gmail.com>
2023-10-04 00:45:22 +0530

    Implemented show, delete and update functionality

--- controllers/categories.controller.js
+++ controllers/categories.controller.js
@@ -12,6 +12,25 @@ const list = (req, res) => {
     );
 };
 
+const show = (req, res) => {
+  Category.findById(req.params.id)
+    .then((doc) => {
+      if (doc) {
+        res.status(HttpStatusCodes.StatusCodes.OK).send(doc);
+      } else {
+        res.status(HttpStatusCodes.StatusCodes.NOT_FOUND).send({
+          message: "Internal Server Error",
+          error: "Resource Not Found",
+        });
+      }
+    })
+    .catch((err) =>
+      res
+        .status(HttpStatusCodes.StatusCodes.INTERNAL_SERVER_ERROR)
+        .send({ message: "Internal Server Error", error: err })
+    );
+};
+
 const create = (req, res) => {
   Category.create(req.body)
     .then((doc) => res.status(HttpStatusCodes.StatusCodes.CREATED).send(doc))
@@ -22,7 +41,44 @@ const create = (req, res) => {
     );
 };
 
+const update = (req, res) => {
+  Category.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    .then((doc) => {
+      if (doc) {
+        res.status(HttpStatusCodes.StatusCodes.OK).send(doc);
+      } else {
+        res.status(HttpStatusCodes.StatusCodes.NOT_FOUND).send({
+          message: "Internal Server Error",
+          error: "Resource Not Found",
+        });
+      }
+    })
+    .catch((err) =>
+      res
+        .status(HttpStatusCodes.StatusCodes.INTERNAL_SERVER_ERROR)
+        .send({ message: "Internal Server Error", error: err })
+    );
+};
+
+const destroy = (req, res) => {
+  Category.findByIdAndDelete(req.params.id)
+    .then((doc) => {
+      if (doc) {
+        res.status(HttpStatusCodes.StatusCodes.OK).send({ message: "Deleted" });
+      } else {
+        res.status(HttpStatusCodes.StatusCodes.NOT_FOUND).send({
+          message: "Internal Server Error",
+          error: "Resource Not Found",
+        });
+      }
+    })
+    .catch((err) => console.log(err));
+};
+
 module.exports = {
   list,
   create,
+  show,
+  update,
+  destroy,
 };
--- routes/category.routes.js
+++ routes/category.routes.js
@@ -5,6 +5,9 @@ const router = express.Router();
 const CategoriesController = require("../controllers/categories.controller");
 
 router.route("/").get(CategoriesController.list);
+router.route("/:id").get(CategoriesController.show);
 router.route("/").post(CategoriesController.create);
+router.route("/:id").put(CategoriesController.update);
+router.route("/:id").delete(CategoriesController.destroy);
 
 module.exports = router;
